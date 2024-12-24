2e9b1b80f30bed4cab88ba5bd2eb9a569ac293db
Sivagnanalingam Yathavan <yathava-se20015@stu.kln.ac.lk>
2024-12-24 10:47:18 +0530

    Homepg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from '../src/components/Home';
 
 function App() {
  
 
   return (
-    <>
-      
-         
-    </>
+    <router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+
+       
+      </Routes>
+    </router>
   )
 }
 
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Logo, NavigationLinks, NavLink, ButtonsContainer, NavLink, ButtonsContainer, LoginButton, GuestButton, HomeContainer, SchoolInfo,SchoolImage, Title, LoremTextContainer,AdminRegisterLink } from '../styles/styles';
+import { Navbar, Logo, NavigationLinks, NavLink, ButtonsContainer, LoginButton, GuestButton, HomeContainer, SchoolInfo,SchoolImage, Title, LoremTextContainer,AdminRegisterLink } from '../styles/styles';
 import {LoremIpsum} from 'lorem-ipsum';
 import bg from '../assets/bg.jpg';
 import bg1 from '../assets/bg1.jpg';
@@ -10,7 +10,7 @@ const lorem = new LoremIpsum();
 
 const Home = ()=> {
     const navigate = useNavigate();
-    const LoremText = lorem.generateParagraph(1);
+
 
     const handleLoginClick = () => {
 
@@ -20,11 +20,29 @@ const Home = ()=> {
         <Navbar>
             <Logo src={bg1} alt ='Logo' />
             <NavigationLinks>
-            <NavLink>About </NavLink>
-
-
+            <NavLink href='#'>About us</NavLink>
+            <NavLink href='#'>Products</NavLink>
+            <NavLink href='#'>Contact us</NavLink>
             </NavigationLinks>
+            <ButtonsContainer>
+                <LoginButton>SignIn</LoginButton>
+                <GuestButton>Guest Mode</GuestButton>
+            </ButtonsContainer>
         </Navbar>
+        <HomeContainer>
+            <SchoolInfo>
+                <Title>School Management System</Title>
+                {/* <LoremTextContainer>
+                    <p>
+                        {LoremText}
+                    </p>
+                </LoremTextContainer> */}
+                <AdminRegisterLink>Admin Register</AdminRegisterLink>
+            </SchoolInfo>
+            <SchoolImage src= {bg} alt="pupils"/>
+        </HomeContainer>
         </>
     )
-}
+};
+
+export default Home;
